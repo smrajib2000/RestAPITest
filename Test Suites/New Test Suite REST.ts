c03583102e15ed237c13814e4b5e25b3f65d8d03
Rajib <Rajib@DESKTOP-R31J20A>
2020-05-11 01:15:15 -0400

    Test

---
+++ Test Suites/New Test Suite REST.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite REST</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>5d3b3316-07f7-4ab2-adfa-edd20ce110df</testSuiteGuid>
+   <testCaseLink>
+      <guid>041f5f28-9740-4b0b-baf5-582d02717709</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ListUsersTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>59328a83-f927-4cbd-a090-68fa48ebc7c6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTestCase/Addition</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
